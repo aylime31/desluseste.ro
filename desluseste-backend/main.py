8b8c7633914744b1b4f512c100fdb84459321701
aylime31 <121061385+aylime31@users.noreply.github.com>
2025-10-23 23:12:21 +0300

    api key update

--- desluseste-backend/main.py
+++ desluseste-backend/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
-# --- SETUP ---
+
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
